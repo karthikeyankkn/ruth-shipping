cdc031956c968f4e48b8cd8b680840565fe17462
karthikeyankkn <connecttokkn@hotmail.com>
2019-06-03 16:51:27 +0530

    added complete login logout function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  
+  root 'sessions#new'
+
+  get 'sessions/new'
   get 'test/new'
 
+  #session routes
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  # user routes
   resources :users
   post '/signup',  to: 'users#create'
 end
