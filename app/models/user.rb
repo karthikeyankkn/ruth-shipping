3552e027ddd8333e28c76b2ae26eefd9190f46ef
karthikeyankkn <connecttokkn@hotmail.com>
2019-06-14 17:28:48 +0530

    added edit and view

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
 
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  
   class << self
     # Returns the hash digest of the given string.
     def digest(string)
