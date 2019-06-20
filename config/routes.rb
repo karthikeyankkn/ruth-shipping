e91f744bfa0d5e31ad31a535cb6dd7a6a30104e6
karthikeyankkn <connecttokkn@hotmail.com>
2019-06-20 15:47:16 +0530

    added password reset option

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root 'sessions#new'
 
   get 'sessions/new'
@@ -14,4 +16,5 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   post '/signup',  to: 'users#create'
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
