6eb4daa9588ad5a26f9a86cac2b74bfb1e46fa0c
karthikeyankkn <connecttokkn@hotmail.com>
2019-07-30 18:42:22 +0530

    added pre compile false

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module MarketingTools
 		  'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
 		}
 
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
