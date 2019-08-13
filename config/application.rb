082bb3925e06490f6eb09e42ca9df5f6edc77358
karthikeyankkn <connecttokkn@hotmail.com>
2019-08-13 12:14:36 +0530

    added asset minification false

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module MarketingTools
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.assets.initialize_on_precompile = false
     config.action_dispatch.default_headers = {
 		  'Access-Control-Allow-Origin' => '*',
 		  'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
